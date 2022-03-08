5bc42d0f585d59deb69c5e24d9ff10ba74532e39
Bruno Castagnino Rossi <bcastagninorossi@udesa.edu,ar>
2022-03-08 14:29:18 -0300

    mensaje

---
+++ PRACTICAS CON ALVARO/Variables.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+# var1 = 10
+# var2 = "Hello world"
+# var3 = None
+# var4 = 7.5
+# var5 = True
+
+# print (type(var1))
+# print (type(var2))
+# print (type(var3))
+# print (type(var4))
+# print (type(var5))
+
+
+###
+
+# def desambiguator (premise):
+#     if premise == False:
+#         print ("Tenías razón, era mentira")
+#     if premise == 1:
+#         print ("Era verdad!")
+    
+# desambiguator(1)
+
+###
+
+# x1 = 100
+# x2 = 20
+# x3 = -5
+
+# y = x1+x2+x3
+
+# z= x1-x2*x3
+
+# w = (x1+x2+x3)-(x1-x2*x3)
+
+# print(w)
+
+###
+
+# num = 23
+# data = True
+# var = 40.0
+# res1 = num+data
+# res2 = data/var
+# res3 = num*var
+
+###
+
+# num = 23
+# data = False
+# var = 40.0
+# res1 = num+data
+# res2 = data/var
+# res3 = num*var
+
+###
+# result = 70
+# data = False
+# value = '158'
+# var1 = result * data
+# var2 = data + value
+# var3 = result + value
+
+###
+
+# result = 5
+# value = '158'
+# location = 'payunia'
+# ##acá hicimos estas operaciones en la consola para concatenar y multiplicar strings
+# ##var1 = result*value   var2 = value+location
+
+
+###
+
+#Listas
+# list1 = [algo,otra cosa, etc]
+
+###
+
+# name = 'Mike '
+# phrase = 'needs a coffee'
+# full_phrase = name + phrase
+# extended_phrase = full_phrase + ', urgente!'
+# #Las concatenaciones enlentecen la ejecución del código, hay que tratar de minimizarlas
+
+# subnet = '192.168.0'
+# host = '34'
+# ip = subnet + '.' + host
+
+###
+
+# a = 50
+# b = 6
+# c = 8 
+# #d = 2*a + 1/(b-5*c)
+
+# d = ((a*b+c)/(2-a)+((a*b+c)/(2-a)+2)/(c+b))*(1+(a*b+c)/(2-a))
+
+# print(d)
+
+###
+
+# word0 = 'Life'
+# word1 = 'ocean'
+# word2 = 'up'
+# word3 = 'down'
+
+# word0 = 'Mars'
+# word1 = 'earth'
+# word2 = 'round'
+# word3 = 'round'
+
+# word4 = word0+ ' is like the ' + word1 + ', it goes ' + word2 + ' and ' + word3 
+# print(word4)
+
+###
+
+
+
